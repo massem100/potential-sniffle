565a2451bce8ff8dfac70dfbbdfeeac95766fc3c
massem100 <51300224+massem100@users.noreply.github.com>
2021-12-06 22:45:21 -0500

    small update

--- static/js/count.js
+++ static/js/count.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 31, 2021 12:00:00").getTime();
+var countDownDate = new Date("Dec 31, 2021 24:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -55,6 +55,6 @@ var x = setInterval(function() {
   // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("count-new").innerHTML = "IT's HERE..";
+    document.getElementById("count-new").innerHTML = "IT'S HERE..";
   }
 }, 1000);
